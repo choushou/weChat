b9cb6be39f77b8d5136180b780043699ea5ce767
XMG <XMG@163.com>
2016-07-19 02:54:59 +0900

    初始化项目

---
+++ wechat/wechat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wechat
+//
+//  Created by ios on 16/7/19.
+//  Copyright © 2016年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
